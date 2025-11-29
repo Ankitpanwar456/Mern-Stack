9d83b0c60db9e3160b621a75b698d991c03d8d2c
Ankit Panwar <111564454+Ankitpanwar456@users.noreply.github.com>
2025-11-29 10:34:40 +0530

    Add files via upload

---
+++ JavaScript/Class_01/script.js
@@ -0,0 +1,24 @@
+function higher_order_function(call_back_function)
+{
+  console.log("this is higher order")
+}
+
+
+function call_back_function()
+{
+    let a=10;
+    let b=20;
+    let sum;
+   return  sum=a+b;
+    
+
+}
+
+function higherfunction(demo)
+{
+    console.log(sum);
+}
+
+higherfunction(call_back_function);
+
+
